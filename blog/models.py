ba599b4c1c5a0341d5c8a11dfb7eb3baa0529978
itsduhnay <rebeccadsimmons@gmail.com>
2026-01-06 10:31:30 -0500

    Added bootstrap and completed jobs url

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    summary = models.TextField(blank=True, default='')
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    
+
+    def __str__(self):
+        return self.title
